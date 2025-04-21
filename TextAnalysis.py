4b8bf93044282da4829ad760796946cf5cfc1482
albiecr <albertinacrodrigues@gmail.com>
2025-04-21 17:14:51 -0300

    feat(menu): add option to select between all words or specific word frequency

--- TextAnalysis.py
+++ TextAnalysis.py
@@ -39,12 +39,15 @@ analyzed = TextAnalyzer(givenstring)
 
 # Call the function that converts the data into lowercase
 print("Formatted Text: ", analyzed.fmtText)
+opcao = input("Enter the function number you want to use, then press Enter:\n1 Count of occurrences of all words\n2 Count of occurrences of a specific word\n")
 
-# Call the function that counts the frequency of all unique words from the data
-freqMap = analyzed.freqAll()
-print(freqMap)
+if opcao == '1':
+    # Call the function that counts the frequency of all unique words from the data
+    freqMap = analyzed.freqAll()    
+    print("Here is the frequency count of all words in the provided text: ", freqMap)
+else:
+    # Call the function that counts the frequency of a specific word
+    word = input("Please type the word that you want to be counted on the text: ")
+    frequency = analyzed.freqOf(word)
+    print("The word ", word, "appears ", frequency, "times.")
 
-# Call the function that counts the frequency of a specific word
-word = input("Please type the word that you want to be counted on the text: ")
-frequency = analyzed.freqOf(word)
-print("The word ", word, "appears ", frequency, "times.")
